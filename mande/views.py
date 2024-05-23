5f95f617a0d4c5a1d7b0f5bce2963b7925e5dad1
Abdulsaboorhabibi <saboorhabibi10@gmail,com>
2024-05-23 15:11:20 +0430

    black is added.

--- mande/views.py
+++ mande/views.py
@@ -8,7 +8,9 @@ from django.core.paginator import Paginator
 from . import models
 from . import project_form
 
-# list of all Projects 
+# list of all Projects
+
+
 class ListProject(LoginRequiredMixin, ListView):
     paginate_by = 1
     model = models.Project
@@ -17,37 +19,43 @@ class ListProject(LoginRequiredMixin, ListView):
         context = super().get_context_data(**kwargs)
         context["count"] = models.Project.objects.count()
         context["using"] = models.Project.objects.filter(overall_Status="C").count()
-        context["Processing"] = models.Project.objects.filter(overall_Status="P").count()
+        context["Processing"] = models.Project.objects.filter(
+            overall_Status="P"
+        ).count()
         context["Pendding"] = models.Project.objects.filter(overall_Status="PN").count()
         return context
-    
 
-# detail project 
+
+# detail project.
+
+
 class DetailProject(DetailView):
     model = models.Project
     paginate = 1
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         project = self.get_object()
         monitors = models.MonitorProject.objects.filter(project=project)
         paginator = Paginator(monitors, self.paginate)
 
-        page_number = self.request.GET.get('page')
+        page_number = self.request.GET.get("page")
         page_obj = paginator.get_page(page_number)
 
-        context['page_obj'] = page_obj
+        context["page_obj"] = page_obj
         return context
-    
+
+
 # create new project view
+
+
 class CreateProject(CreateView):
     template_name = "mande/create_project.html"
     model = models.Project
-    #fields = "__all__"
+    # fields = "__all__"
     form_class = project_form.ProjectForm
-    success_url = '/mande/'
+    success_url = "/mande/"
 
     def form_valid(self, form):
         # form
         return super().form_valid(form)
-    
